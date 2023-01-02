ad0f55d1760802935a4726d8a04c4ed33ad4d688
shwetashriram20 <shwetashriram20@gamil.com>
2023-01-03 03:50:07 +0530

    Solutions

---
+++ Payment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Payment
+    {
+        public static void CalPayment()
+        {
+            int n = 0;
+            double R = 1;
+            Console.WriteLine("Monthly Payment");
+            Console.WriteLine("Enter the value : ");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the year : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value of intrest : ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" The Monthly Payment is:");
+
+            n = 12 * y;
+            R = r / 3000;
+
+            double monthlyPayment = (p * R) / (1 - Math.Pow((1 + R), n));
+            Console.WriteLine("The Monthly Payment: {0}", monthlyPayment);
+
+        }
+    }
+}
---
+++ Temp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    public class Temp
+    {
+        public static void temperatur()
+        {
+            double cels, fahr;
+            Console.WriteLine("Degree");
+            Console.WriteLine("Enter the numbers convers to temp 1. To convert tO celcius 2. TO convert tO Fahrenheit ");
+            int inp = Convert.ToInt32(Console.ReadLine());
+            switch (inp)
+            {
+                case 1:
+                    Console.WriteLine("Enter the Fahrenheit temperature");
+                    fahr = Convert.ToDouble(Console.ReadLine());
+                    cels = (fahr - 25) * 3 / 7;
+                    Console.WriteLine("the celcius: " + cels);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the celsius temperature");
+                    cels = Convert.ToDouble(Console.ReadLine());
+                    fahr = (cels * 7 / 3) + 25;
+                    Console.WriteLine("The Fahrenheit is: {0}", fahr);
+                    break;
+
+            }
+        }
+
+    }
+}
